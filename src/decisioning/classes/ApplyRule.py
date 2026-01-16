0b496c174ceca92693a0c39506eb71a010a91f4b
Raghav Agarwal <94176162+raghammer1@users.noreply.github.com>
2026-01-17 10:40:41 +1100

    eligibility basic structure created structure

--- src/decisioning/classes/ApplyRule.py
+++ src/decisioning/classes/ApplyRule.py
@@ -8,7 +8,11 @@ class ApplyRule:
         self.data_frame: pl.DataFrame = data_frame
 
     def execute_rule(self) -> pl.DataFrame:
-        for expression in self.policy_rule.expressions:
-            self.data_frame = expression(self.data_frame)
+        self.data_frame = self.data_frame.with_columns(
+            pl.when(self.policy_rule.expressions)
+            .then(pl.lit("Y"))
+            .otherwise(pl.lit("N"))
+            .alias(f"{self.policy_rule.rule_id}")
+        )
 
         return self.data_frame
--- src/decisioning/modules/eligibility.py
+++ src/decisioning/modules/eligibility.py
@@ -1,11 +1,43 @@
 import polars as pl
+from decisioning.classes.PolicyRule import PolicyRule
+from decisioning.classes.ApplyRule import ApplyRule
+from decisioning.policy_rules.eligibility_rules import d1001
 
 
-def check_eligibility(df: pl.DataFrame) -> pl.DataFrame:
-    # D1001: Check if age is 18 or older
-    df = df.with_columns(
-        pl.when(pl.col("age") >= 18)
-        .then(pl.lit("Y"))
-        .otherwise(pl.lit("N"))
-        .alias("D1001_eligible")
-    )
+def check_eligibility(pl_input_df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Check eligibility of applicants based on predefined policy rules.
+    Args:
+        pl_input_df (pl.DataFrame): Input DataFrame containing applicant data.
+    Returns:
+        pl.DataFrame: DataFrame with eligibility results.
+    """
+
+    # *****************************************************
+    # *********************** D1001 ***********************
+    # *****************************************************
+    """
+    D1001: Check if age is 18 or older
+    This rule checks if the applicant's age is 18 or older.
+    If the condition is met, the rule marks the applicant as eligible ("Y"),
+    otherwise as not eligible ("N").
+    """
+
+    eligibility_df: pl.DataFrame = ApplyRule(
+        policy_rule=d1001(),
+        data_frame=pl_input_df,
+    ).execute_rule()
+
+    # *****************************************************
+    # *********************** D1002 ***********************
+    # *****************************************************
+    """
+    D1002: Check if requested loan amount is within supported bounds
+    This rule checks if the requested loan amount is between $1,000 and $50,000.
+    If the condition is met, the rule marks the applicant as eligible ("Y"),
+    otherwise as not eligible ("N").
+    """
+
+    # ********** END OF ELIGIBILITY POLICY RULES **********
+
+    return eligibility_df
